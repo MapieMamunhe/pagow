13124c6ab7816e7321f98c51a339f58afe375803
Mapie Mamunhe <mmamunhe@gmail.com>
2022-12-19 00:34:16 +0200

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+m; // tailwind.config.js
+
+module.exports = {
+  content: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
